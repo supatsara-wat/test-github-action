c9863d757993dc2a21201aa1a3a756768baf452c
Supatsara <supatsara-wa@MacBook-Pro-80.local>
2023-08-24 17:46:22 +0900

    test4

--- .github/scripts/detect_package_changes.py
+++ .github/scripts/detect_package_changes.py
@@ -1,9 +1,16 @@
 import os
 
+
+def set_github_action_output(output_name, output_value):
+    f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
+    f.write(f'{output_name}={output_value}')
+    f.close()
+
+
 changed_files = os.popen(
     f"git diff --name-only ${{{{ github.event.pull_request.base.sha }}}} ${{{{ github.sha }}}}").read()
 
 if "package.json" in changed_files:
-    print("::set-output name=PACKAGE_JSON_CHANGED::true")
+    set_github_action_output('PACKAGE_JSON_CHANGED', 'true')
 else:
-    print("::set-output name=PACKAGE_JSON_CHANGED::true")
+    set_github_action_output('PACKAGE_JSON_CHANGED', 'true')
